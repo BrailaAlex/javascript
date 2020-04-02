ba535eb99d52e24d9dd2fa32584d45f68645cce5
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-04-02 15:28:05 +0300

    l2ht1

---
+++ Lesson2/hometask1/index.js
@@ -0,0 +1,8 @@
+let b = 2;
+let a = 4;
+console.log('a: ', a);
+console.log('b: ', b);
+console.log('a-b= ', a - b);
+console.log('a+b= ', a + b);
+console.log('a/b= ', a / b);
+console.log('a*b= ', a * b);
