7e8749f8172fcd304f244c212e27a74f3182c32e
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-04-06 21:56:02 +0300

    l4ht2

--- Lesson4.Cycles/hm2/index.js
+++ Lesson4.Cycles/hm2/index.js
@@ -1,6 +1,7 @@
-const m = 5;
-const n = 25;
+const m = 1;
+const n = 18;
 let result = 0;
+
 for (let i = m; i <= n; i++) {
     if (i % 2 === 0 && i % 4 !== 0) {
         result += i;
@@ -12,9 +13,4 @@ for (let i = m; i <= n; i++) {
     if (i % 5 === 0) {
         console.log(i);
     }
-    // else {
-    // // //     continue;
-    // // // }
 }
-
-// console.log(result);
