d24805bd1585aeabaebe0c1984a5ec46aeb2a9b7
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-04-06 21:20:54 +0300

    l4ht1

--- Lesson4.Cycles/hm1/index.js
+++ Lesson4.Cycles/hm1/index.js
@@ -1,6 +1,14 @@
-const n = 20;
-let sum = '1';
+// const n = 20;
+// let sum = '1';
 
-for (let i = '2'; i <= n; i++) {
-    sum += i.toString();
+// for (let i = 2; i <= n; i++) {
+//     sum += i.toString();
+// }
+// console.log(sum);
+const n = 17;
+let sum = '';
+let a = 1;
+while (a <= n) {
+    console.log(sum += a);
+    a++;
 }
