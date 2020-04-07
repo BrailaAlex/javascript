1296be32647a25647c6413d04d17bc2dcf138865
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-04-07 13:30:42 +0300

    l5t3

---
+++ Lesson5_Functions/task3/index.js
@@ -0,0 +1,10 @@
+/* return statement */
+
+function getEvenOdd(num) {
+    if (num % 2 === 0) {
+        return 'even';
+    } else {
+        return 'odd';
+    }
+}
+const result = getEvenOdd(20);
