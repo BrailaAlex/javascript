7d68c3bcbbfcf9e7c1aa65aa6fe5d990ab56212c
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-04-07 14:23:30 +0300

    l5

--- Lesson5_Functions/l5hw1/index.js
+++ Lesson5_Functions/l5hw1/index.js
@@ -1,9 +1,9 @@
 let result = 0
 
 function getSum(a, b) {
-    for (i = a; i <= b; i++) {
-        if (i % 2 === 0) {
-            result += i
+    for (a; a <= b; a++) {
+        if (a % 2 === 0) {
+            result += a
         }
     }
     return result;
