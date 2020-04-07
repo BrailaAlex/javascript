3e81f4d02a7bc7806746202f922001c99d222577
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-04-07 17:12:44 +0300

    l5

--- Lesson5_Functions/l5hw1/index.js
+++ Lesson5_Functions/l5hw1/index.js
@@ -9,3 +9,15 @@ function getSum(a, b) {
     return result;
 }
 getSum(4, 18)
+
+function getSum(a, b) {
+    let sum = 0;
+    for (let i = a; i <= b; i++) {
+        if (i % 2 == 0) {
+            sum += i;
+        }
+    }
+    return sum;
+}
+
+// console.log(getSum(5, 25));
