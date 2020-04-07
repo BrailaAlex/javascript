935c0d656b0943f1e5b813cb144d8b4918cd904c
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-04-07 18:00:42 +0300

    l5

--- Lesson5_Functions/hw5/index.js
+++ Lesson5_Functions/hw5/index.js
@@ -4,6 +4,8 @@ function findDivCount(a, b, n) {
     for (let i = a; i <= b; i++) {
         if (i % n === 0) {
             result++;
+        } else {
+            return 0;
         }
     }
     return result;
