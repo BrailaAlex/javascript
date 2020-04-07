8a053a1dcfe2dde93ef39cc507f64dd0dfd276e1
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-04-07 16:29:52 +0300

    l5

---
+++ Lesson5_Functions/hw4/index.js
@@ -0,0 +1,17 @@
+function compareSums(a, b, c, d) {
+    if (firstSum > secondSum) {
+        return true;
+    } else {
+        false
+    }
+}
+
+function firstSum(a, b) {
+    return a + b;
+}
+
+function secondSum(c, d) {
+    return c + d;
+}
+
+compareSums(4, 3, 7, 1);
