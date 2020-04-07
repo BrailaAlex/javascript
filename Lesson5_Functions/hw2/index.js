4eb7acce97abc1910a1b8ece63752efa3c4d1e65
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-04-07 17:39:12 +0300

    l5

--- Lesson5_Functions/hw2/index.js
+++ Lesson5_Functions/hw2/index.js
@@ -14,7 +14,7 @@
 // getPrimes(35);
 
 function getPrimes(n) {
-    step1: for (let i = 1; i <= n; i++) {
+    step1: for (let i = 2; i <= n; i++) {
         for (let j = 2; j < i; j++) {
             if (i % j === 0) {
                 continue step1;
@@ -24,18 +24,3 @@ function getPrimes(n) {
     }
 }
 getPrimes(35);
-
-// function getPrimes(num) {
-//     for (let i = 1; i <= num; i++) {
-//         if (isPrime(i)) console.log(i);
-//     }
-// }
-
-// function isPrime(num) {
-//     for (let j = 2; j < num; j++) {
-//         if (num % j === 0) return false;
-//     }
-//     return true;
-// }
-
-// getPrimes(100);
