1023f728e80e11bfd2412f0f597408bf7840d1ff
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-04-09 19:33:33 +0300

    l6

--- Lesson6.Massives/task2/index.js
+++ Lesson6.Massives/task2/index.js
@@ -8,14 +8,27 @@
 // }
 // const numbers = [11, 22, 44, 55, 66];
 
-function getSum(arr) {
-    if (!Array.isArray(arr)) return null;
+// function getSum(arr) {
+//     if (!Array.isArray(arr)) return null;
 
-    let sum = 0;
-    for (let num of numbers) {
-        sum += num;
-    }
-    return sum;
-}
+//     let sum = 0;
+//     for (let num of numbers) {
+//         sum += num;
+//     }
+//     return sum;
+// }
 
 // console.log('result: ' + getSum(numbers));
+function getSum(arr) {
+    let sum = 0;
+
+    if (!Array.isArray(arr)) {
+        return null;
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+
+    return sum;
+}
