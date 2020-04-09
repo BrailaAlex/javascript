73376c17509d8132bc874e1efc33f05e82bc1d39
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-04-09 19:32:24 +0300

    l6

--- Lesson6.Massives/task2/index.js
+++ Lesson6.Massives/task2/index.js
@@ -12,9 +12,10 @@ function getSum(arr) {
     if (!Array.isArray(arr)) return null;
 
     let sum = 0;
-    for (let num of numbers)
+    for (let num of numbers) {
         sum += num;
+    }
     return sum;
 }
 
-// console.log('result: ' + getSum(numbers));
+console.log('result: ' + getSum(numbers));
