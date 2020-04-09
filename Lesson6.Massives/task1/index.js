be1ca2039c0a69dfd890f41813b890a6c9db15bd
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-04-09 18:18:26 +0300

    l6

--- Lesson6.Massives/task1/index.js
+++ Lesson6.Massives/task1/index.js
@@ -12,11 +12,11 @@
 
 const numbers = [11, 22, 44, 55, 66];
 
-function getArrayBound(arr) {
+function getArrayBounds(arr) {
     if (!Array.isArray(arr)) {
         return null;
     }
     return [arr.length, arr[0], arr[arr.length - 1]];
 }
 
-const arrayBound = getArrayBound(numbers);
+getArrayBounds(numbers);
