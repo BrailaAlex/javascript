366df9498e84457b2bebac23152844bef3744948
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-04-09 18:40:19 +0300

    l6

--- Lesson6.Massives/task2/index.js
+++ Lesson6.Massives/task2/index.js
@@ -1,22 +1,20 @@
 // Перебор массива
-const numbers = [11, 22, 44, 55, 66];
+
 
 // let sumOfNumbers = 0;
 // for (let i = 0; i < numbers.length; i++) {
 //     console.log(numbers[i]);
 //     sumOfNumbers += numbers[i];
 // }
+const numbers = [11, 22, 44, 55, 66];
 
 function getSum(arr) {
-    if (!Array.isArray(arr)) {
-        return null;
-    }
-    let sum = 0;
-    // for (let num of numbers) { // как альтернатива для варианта ниже // 
-    for (let i = 0; i < numbers.length; i++) {
+    if (!Array.isArray(arr)) return null;
 
+    let sum = 0;
+
+    for (let i = 0; i < numbers.length; i++)
         sum += numbers[i];
-    }
     return sum;
 }
 
