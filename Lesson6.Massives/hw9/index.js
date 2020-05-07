1d4b64e816e5d58a03eb05bfe5b9f624bf2000b6
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-07 19:43:47 +0300

    L6

--- Lesson6.Massives/hw9/index.js
+++ Lesson6.Massives/hw9/index.js
@@ -1,12 +1,12 @@
 function withdraw(clients, balances, client, amount) {
     for (let i = 0; i < clients.length; i++) {
         if (clients[i] == client) {
-            if (balances[i] - amount >= 0) {
-                return balances[i] - amount;
+            if (balances[i] - amount < 0) {
+                return '-1';
             }
-            return '-1';
+            return balances[i] - amount;
         }
     }
 }
 
-console.log(withdraw(['Ann', 'John', 'Bob'], [1500, 600, 75], 'Bob', 85))
+console.log(withdraw(['Ann', 'John', 'Bob'], [1500, 600, 75], 'Bob', 65))
