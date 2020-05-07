33e58fb102314e548cc9d4fd12279080fe1ccbbe
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-07 11:28:12 +0300

    L6

--- Lesson6.Massives/hw5/index.js
+++ Lesson6.Massives/hw5/index.js
@@ -3,7 +3,7 @@
 const numbers = [11, 22, 44, 55, 66];
 
 function reverseArray(arr) {
-    if (!Array.isArray) return null;
+    if (!Array.isArray(arr)) return null;
     let swapArr = []
     for (let i = 1; i <= arr.length; i++) {
         swapArr.push(arr[arr.length - i]);
