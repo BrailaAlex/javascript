0f367c74dcaa3ebed109524eadf9c11945f35c9d
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-08 02:26:27 +0300

    L6

--- Lesson6.Massives/hw13/index.js
+++ Lesson6.Massives/hw13/index.js
@@ -1,30 +1,30 @@
-// function uniqueCount(array) {
-//     if (!Array.isArray(array)) return null;
-//     const uniqueSet = [...new Set(array)];
-//     return uniqueSet.length;
-// }
-// console.log(removeDuplicates([25, 65, 32, 65, 95, 75, 85, 95]));
 function uniqueCount(array) {
     if (!Array.isArray(array)) return null;
-    let uniqueArr = [];
-
-    for (let i = 0; i < array.length; i++) {
-
-        let unique = false;
-
-        for (let j = 0; j < uniqueArr.length; j++) {
-            if (array[i] == uniqueArr[j]) {
-                unique = true;
-            }
-        }
-        if (!unique) {
-            uniqueArr.push(array[i]);
-        }
-    }
-
-    return uniqueArr.length - 1;
+    const uniqueSet = [...new Set(array)];
+    return uniqueSet.length;
 }
 console.log(uniqueCount([25, 65, 32, 65, 95, 75, 85, 95]));
+// function uniqueCount(array) {
+//     if (!Array.isArray(array)) return null;
+//     let uniqueArr = [];
+
+//     for (let i = 0; i < array.length; i++) {
+
+//         let unique = false;
+
+//         for (let j = 0; j < uniqueArr.length; j++) {
+//             if (array[i] == uniqueArr[j]) {
+//                 unique = true;
+//             }
+//         }
+//         if (!unique) {
+//             uniqueArr.push(array[i]);
+//         }
+//     }
+
+//     return uniqueArr.length - 1;
+// }
+// console.log(uniqueCount([25, 65, 32, 65, 95, 75, 85, 95]));
 
 // function removeDuplicates(array) {
 
