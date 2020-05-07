7672875b568c0e38ecc6ff219aa0096368de940b
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-07 11:39:25 +0300

    L6

--- Lesson6.Massives/hw6/index.js
+++ Lesson6.Massives/hw6/index.js
@@ -5,7 +5,7 @@ const numbers = [11, 22, 44, 55, 66];
 function cloneArr(arr) {
     if (!Array.isArray(arr)) return null;
     let newArr = []
-    for (let i = 0; i <= arr.length; i++) {
+    for (let i = 0; i <= arr.length - 1; i++) {
         newArr.push(arr[i]);
     }
     return newArr;
