7aea1e2dc9e6af72241b26373540d6a370e316e0
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-07 11:26:45 +0300

    L6

---
+++ Lesson6.Massives/hw5/index.js
@@ -0,0 +1,14 @@
+// spread и деструктуризация
+
+const numbers = [11, 22, 44, 55, 66];
+
+function reverseArray(arr) {
+    if (!Array.isArray) return null;
+    let swapArr = []
+    for (let i = 1; i <= arr.length; i++) {
+        swapArr.push(arr[arr.length - i]);
+    }
+    return swapArr;
+}
+
+console.log(reverseArray(numbers));
