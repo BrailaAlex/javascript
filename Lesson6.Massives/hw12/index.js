7e1b6f75d7492054b81860f47e19f2c692e8daad
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-08 00:03:15 +0300

    L6

--- Lesson6.Massives/hw12/index.js
+++ Lesson6.Massives/hw12/index.js
@@ -1,29 +1,29 @@
-function removeDublicates(array) {
-    if (!Array.isArray) return null;
-    const uniqueSet = [...new Set(array)];
-    return uniqueSet;
-}
-// console.log(removeDublicates([25, 65, 32, 65, 95, 75, 85, 95]));
-// function uniqueCount(array) {
-
-//     let uniqueArr = [];
-
-//     for (let i = 0; i < array.length; i++) {
-
-//         let unique = false;
-
-//         for (let j = 0; j < uniqueArr.length; j++) {
-//             if (array[i] == uniqueArr[j]) {
-//                 unique = true;
-//             }
-//         }
-//         if (!unique) {
-//             uniqueArr.push(array[i]);
-//         }
-//     }
-
-//     return uniqueArr
+// function removeDublicates(array) {
+//     if (!Array.isArray) return null;
+//     const uniqueSet = [...new Set(array)];
+//     return uniqueSet;
 // }
+// console.log(removeDublicates([25, 65, 32, 65, 95, 75, 85, 95]));
+function removeDublicate(array) {
+
+    let uniqueArr = [];
+
+    for (let i = 0; i < array.length; i++) {
+
+        let unique = false;
+
+        for (let j = 0; j < uniqueArr.length; j++) {
+            if (array[i] == uniqueArr[j]) {
+                unique = true;
+            }
+        }
+        if (!unique) {
+            uniqueArr.push(array[i]);
+        }
+    }
+
+    return uniqueArr
+}
 
 // function uniqueCount(array) {
 
