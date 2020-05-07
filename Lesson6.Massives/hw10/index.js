16311a87f17d1eb10ca0f13ebf2a68a8aa62e4dc
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-07 20:03:10 +0300

    L6

--- Lesson6.Massives/hw10/index.js
+++ Lesson6.Massives/hw10/index.js
@@ -1,9 +1,9 @@
 function getSubArray(arr, n) {
     let subArr = [];
-    for (let i = 0; i < n; i++) {
+    for (let i = 1; i <= n; i++) {
         subArr.push(arr[i]);
     }
     return subArr;
 }
 
-// console.log(getSubArray([25, 32, 65, 75, 85, 95], 3));
+console.log(getSubArray([25, 32, 65, 75, 85, 95], 5));
