86fe103bc17e33f2c591456fed9b521cffff295c
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-07 11:37:48 +0300

    L6

--- Lesson6.Massives/hw6/index.js
+++ Lesson6.Massives/hw6/index.js
@@ -1,14 +1,14 @@
 // spread и деструктуризация
 
-// const numbers = [11, 22, 44, 55, 66];
+const numbers = [11, 22, 44, 55, 66];
 
 function cloneArr(arr) {
     if (!Array.isArray(arr)) return null;
-    let cloneArray = []
+    let newArr = []
     for (let i = 0; i <= arr.length; i++) {
-        cloneArray.push(arr[i]);
+        newArr.push(arr[i]);
     }
-    return cloneArray;
+    return newArr;
 }
 
-// console.log(cloneArr(numbers));
+console.log(cloneArr(numbers));
