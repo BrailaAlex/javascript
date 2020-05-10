8bcbb54cb109078af88b3cfdb726e2b648c98db1
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-10 19:55:52 +0300

    L7

---
+++ Lesson7/task3/index.js
@@ -0,0 +1,18 @@
+// [1, [2, 3, 4], 5, 6] => [1, 2, 3, 4, 5, 6]
+
+const flatArray = arr => arr
+    // .flat();
+    .reduce((acc, elem) => acc.concat(elem), []);
+
+const initArray = [1, [2, 3, 4], 5, 6];
+console.log(flatArray(initArray));
+
+
+// [1, 2, 3, 4].reduce(function(acc, num) {
+//     return acc + num;
+// });
+
+// 1: 0, 1 => 1
+// 2: 1, 2 => 3
+// 3: 3, 3 => 6
+// 4: 6, 4 => 10
