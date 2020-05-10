be63c534ef010d0077d7ade8af8a8358e50c0bc9
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-10 19:37:56 +0300

    L7

---
+++ Lesson7/task2/index.js
@@ -0,0 +1,8 @@
+ // unsorted array => sorted array
+
+ const sortDesc = numbers =>
+     numbers.slice().sort((a, b) => b - a);
+
+ //  const arr = [27, 2, 3, 20, 15, 16, 25];
+ //  console.log(sortDesc(arr));
+ //  console.log(arr);
