4e8b4e9c1ae998458305ab75f90440eba4d2d224
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-11 02:38:32 +0300

    L7

---
+++ Lesson7/HW8/index.js
@@ -0,0 +1,6 @@
+function arrAverage(arr) {
+    if (!Array.isArray(arr)) return null;
+    return arr.reduce((acc, elem) => (acc + elem));
+}
+const numbers = [25, 65, 19, 14, 21, 16, 17]
+console.log(arrAverage(numbers) / numbers.length);
