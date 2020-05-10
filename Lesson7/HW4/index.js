1ae2da8a821dc86ba14302a10d372951946a6910
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-11 00:38:08 +0300

    L7

--- Lesson7/HW4/index.js
+++ Lesson7/HW4/index.js
@@ -1,7 +1,4 @@
 function cloneArr(arr) {
     if (!Array.isArray(arr)) return null;
-    return arr.slice();
+    return cloneArr = [...arr];
 }
-numbers = [25, 65, 19, 14, 21, 16, 17];
-console.log(cloneArr(numbers));
-console.log(numbers);
