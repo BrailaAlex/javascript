ad0345218f7325a79ac8f97af7adceb4cf58f634
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-11 17:54:42 +0300

    L8

--- Lesson8/task2/index.js
+++ Lesson8/task2/index.js
@@ -1,25 +1,25 @@
 // concatProps
-const user = {
-    name: 'Tom',
-    key: 17,
-    age: 21,
-};
+
 
 // for (let key in user) {
 //     console.log(user[key]);
 // }
 
 const concatProps = obj => {
-    const arr = [];
+        const arr = [];
 
-    for (let key in obj) {
-        arr.push(obj[key]);
-        // arr = arr.concat(obj[key])
+        for (let key in obj) {
+            arr.push(obj[key]);
+            // arr = arr.concat(obj[key]);
+        }
+
+        return arr;
     }
-
-    return arr;
-}
-
-console.log(concatProps(user));
+    // const user = {
+    //     name: 'Tom',
+    //     key: 17,
+    //     age: 21,
+    // };
+    // console.log(concatProps(user));
 
 // user => ['Tom', 17] // function, that gives back an array with all the properties of object
