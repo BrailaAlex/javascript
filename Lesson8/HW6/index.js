ee87b196fb65225f0e8213be8cac2571f03f538b
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-11 23:15:17 +0300

    L8

---
+++ Lesson8/HW6/index.js
@@ -0,0 +1,29 @@
+mergeObjectsV1 = (obj1, obj2) => {
+    return resultedObj = Object.assign({}, obj1, obj2);
+};
+
+mergeObjectsV2 = (obj1, obj2) => {
+    return resultedObj = Object.assign({}, obj2, obj1);
+};
+
+mergeObjectsV3 = (obj1, obj2) => {
+    return resultedObj = {...obj1, ...obj2 };
+};
+mergeObjectsV4 = (obj1, obj2) => {
+    return resultedObj = {...obj2, ...obj1 };
+};
+
+const obj1 = {
+    name: 'Tom',
+    age: 17,
+};
+const obj2 = {
+    name: 'Bob',
+    student: false,
+};
+
+
+console.log(mergeObjectsV1(obj1, obj2));
+console.log(mergeObjectsV2(obj1, obj2));
+console.log(mergeObjectsV3(obj1, obj2));
+console.log(mergeObjectsV4(obj1, obj2));
