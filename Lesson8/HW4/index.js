78bce7f202cc93707ea121564ccbb8941bb6ab19
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-11 22:25:19 +0300

    L8

--- Lesson8/HW4/index.js
+++ Lesson8/HW4/index.js
@@ -5,23 +5,24 @@
 
  function addPropertyV2(userData, userId) {
      const sourceData = { id: userId };
-     return returnedData = Object.assign(userData, sourceData);
+     return userData = Object.assign(userData, sourceData);
  };
 
  function addPropertyV3(userData, userId) {
      const cloneData = Object.assign({}, userData)
      const sourceData = { id: userId };
-     return returnedData = Object.assign(cloneData, sourceData);
+     returnedData = Object.assign(cloneData, sourceData);
+     return cloneData;
  };
 
  function addPropertyV4(userData, userId) {
-     const cloneData = Object.assign({}, userData)
-     return returnedData = {...cloneData, id: userId };
+     //  const cloneData = Object.assign({}, userData)
+     return returnedData = {...userData, id: userId };
  };
 
  const user = {
      name: 'Add',
  }
- const result = addPropertyV1(user, '3256897414');
+ const result = addPropertyV4(user, '3256897414');
 
  console.log(result);
