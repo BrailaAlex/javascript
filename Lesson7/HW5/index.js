94f169bae4d2ea7dfd067d279ac6dcc0632f0356
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-11 13:50:24 +0300

    L7

--- Lesson7/HW5/index.js
+++ Lesson7/HW5/index.js
@@ -1,3 +1,4 @@
-withdraw = (clients, balances, client, amount) =>
-    (balances[clients.indexOf(client)] < amount) ? -1 : (balances[clients.indexOf(client)] - amount);
-console.log(withdraw(['Ann', 'John', 'Bob'], [1500, 600, 75], 'Bob', 84));
+function withdraw(clients, balances, client, amount) {
+    return (balances[clients.indexOf(client)] < amount) ? -1 : (balances[clients.indexOf(client)] - amount);
+}
+console.log(withdraw(['Ann', 'John', 'Bob'], [1500, 600, 75], 'Bob', 55));
