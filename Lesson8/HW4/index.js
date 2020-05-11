15db7e30b5b7952559915c6612fa174a6dd6959c
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-11 21:45:42 +0300

    HW4

--- Lesson8/HW4/index.js
+++ Lesson8/HW4/index.js
@@ -1,23 +1,27 @@
-addPropertyV1 = (userData, userId) => {
-    userData.id = userId;
-    return userData;
-}
-addPropertyV2 = (userData, userId) => {
-    const sourceData = { id: userId };
-    return returnedData = Object.assign(userData, sourceData);
-}
-addPropertyV3 = (userData, userId) => {
-    const cloneData = Object.assign({}, userData)
-    const sourceData = { id: userId };
-    return returnedData = Object.assign(cloneData, sourceData);
-}
-addPropertyV4 = (userData, userId) => {
-        const cloneData = Object.assign({}, userData)
-        return returnedData = {...cloneData, id: userId };
-    }
-    // const user = {
-    //     name: 'Add',
-    // }
-    // const result = addPropertyV4(user, '3256897414');
+ function addPropertyV1(userData, userId) {
+     userData.id = userId;
+     return userData;
+ };
 
-// console.log(result);
+ function addPropertyV2(userData, userId) {
+     const sourceData = { id: userId };
+     return returnedData = Object.assign(userData, sourceData);
+ };
+
+ function addPropertyV3(userData, userId) {
+     const cloneData = Object.assign({}, userData)
+     const sourceData = { id: userId };
+     return returnedData = Object.assign(cloneData, sourceData);
+ };
+
+ function addPropertyV4(userData, userId) {
+     const cloneData = Object.assign({}, userData)
+     return returnedData = {...cloneData, id: userId };
+ };
+
+ //  const user = {
+ //      name: 'Add',
+ //  }
+ //  const result = addPropertyV4(user, '3256897414');
+
+ //  console.log(result);
