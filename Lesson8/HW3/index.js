2b867cd09f2e37143b5182eedd3599ea0f706ff0
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-11 20:17:51 +0300

    L8

--- Lesson8/HW3/index.js
+++ Lesson8/HW3/index.js
@@ -1,12 +1,13 @@
-getKeys = obj =>
-    Object.keys(obj).forEach(key => console.log(key));
-// const user = {
-//     name: 'Alex',
-//     age: 30,
-//     married: true,
-//     children: 'two girls',
-// }
-
-// getKeys(user);
+function getKeys(obj) {
+    let arrKeys = Object.keys(obj);
+    return (arrKeys.forEach(key => console.log(key)));
+}
+const user = {
+    name: 'Alex',
+    age: 30,
+    married: true,
+    children: 'two girls',
+}
+getKeys(user);
 
 // console.log(getKeys(user).forEach(key => console.log(key)));
