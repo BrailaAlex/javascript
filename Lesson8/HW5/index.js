f654fb45a712524663dd14eed8d0e0a2bee02568
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-12 11:13:02 +0300

    L8

--- Lesson8/HW5/index.js
+++ Lesson8/HW5/index.js
@@ -15,11 +15,28 @@ addPropertyV3 = (obj, key, value) => {
     return Object.assign({}, obj, sourceObj);
 }
 addPropertyV4 = (obj, key, value) => {
-    const sourceObj = {
-        [key]: value
-    };
-    return Object.assign({...obj }, {...sourceObj });
-}
+        const sourceObj = {
+            [key]: value
+        };
+        return Object.assign({...obj }, sourceObj);
+    }
+    // function addPropertyV1(obj, key, value) {    
+    //     obj[key] = value;
+    //     return obj;
+    // }
+    // function addPropertyV2(obj, key, value) {   
+    //      let idUser = {[key]: valu};    
+    //      return Object.assign(obj, idUser);
+    //     }
+    // function addPropertyV3(obj, key, value) {
+    //     let idUser = {[key]: value};
+    //     return Object.assign({}, obj, idUser);
+    // }
+    // function addPropertyV4(obj, key, value) {
+    //     let idUser = {[key]: value};
+    //     return Object.assign({...obj }, idUser);
+    // }
+
 const transaction = {
     value: 170,
 }
