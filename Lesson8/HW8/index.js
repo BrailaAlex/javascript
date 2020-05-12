eff6e6faf46e4e8342135cbe11dc7447ce2ba075
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-12 11:44:53 +0300

    L8

--- Lesson8/HW8/index.js
+++ Lesson8/HW8/index.js
@@ -1,10 +1,10 @@
-const buildObject = (keyList, valuesList) => {
+const buildObject = (keysList, valuesList) => {
     obj = {};
-    keyList.forEach((el, i) => obj[el] = valuesList[i]);
+    keysList.forEach((el, i) => obj[el] = valuesList[i]);
     return obj;
 }
 
-const keyList = ['name', 'surname', 'age'];
+const keysList = ['name', 'surname', 'age'];
 const valuesList = ['Oleksii', 'Braila', 30];
-const result = buildObject(keyList, valuesList);
+const result = buildObject(keysList, valuesList);
 console.log(result);
