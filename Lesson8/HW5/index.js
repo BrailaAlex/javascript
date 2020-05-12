504fd9b002e0a7ef4ae4da399ed4a7ed4e154361
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-12 09:20:45 +0300

    L8

--- Lesson8/HW5/index.js
+++ Lesson8/HW5/index.js
@@ -15,7 +15,10 @@ addPropertyV3 = (obj, key, value) => {
     return returnedData = Object.assign({}, obj, sourceObj);
 }
 addPropertyV4 = (obj, key, value) => {
-    return returnedObj = {...obj, [key]: value };
+    const sourceObj = {
+        [key]: value
+    };
+    return returnedObj = ({...obj }, sourceObj);
 }
 const transaction = {
     value: 170,
