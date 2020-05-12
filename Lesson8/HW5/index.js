cba9516ccc0961fcb24ec6e24c24f756fa4ac105
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-12 11:13:43 +0300

    L8

--- Lesson8/HW5/index.js
+++ Lesson8/HW5/index.js
@@ -1,41 +1,47 @@
-addPropertyV1 = (obj, key, value) => {
+// addPropertyV1 = (obj, key, value) => {
+//     obj[key] = value;
+//     return obj;
+// }
+// addPropertyV2 = (obj, key, value) => {
+//     const sourceObj = {
+//         [key]: value
+//     };
+//     return Object.assign(obj, sourceObj);
+// }
+// addPropertyV3 = (obj, key, value) => {
+//     const sourceObj = {
+//         [key]: value
+//     };
+//     return Object.assign({}, obj, sourceObj);
+// }
+// addPropertyV4 = (obj, key, value) => {
+//         const sourceObj = {
+//             [key]: value
+//         };
+//         return Object.assign({...obj }, sourceObj);
+//     }
+function addPropertyV1(obj, key, value) {
     obj[key] = value;
     return obj;
 }
-addPropertyV2 = (obj, key, value) => {
-    const sourceObj = {
-        [key]: value
-    };
-    return Object.assign(obj, sourceObj);
+
+function addPropertyV2(obj, key, value) {
+    let idUser = {
+        [key]: valu };
+    return Object.assign(obj, idUser);
 }
-addPropertyV3 = (obj, key, value) => {
-    const sourceObj = {
-        [key]: value
-    };
-    return Object.assign({}, obj, sourceObj);
+
+function addPropertyV3(obj, key, value) {
+    let idUser = {
+        [key]: value };
+    return Object.assign({}, obj, idUser);
 }
-addPropertyV4 = (obj, key, value) => {
-        const sourceObj = {
-            [key]: value
-        };
-        return Object.assign({...obj }, sourceObj);
-    }
-    // function addPropertyV1(obj, key, value) {    
-    //     obj[key] = value;
-    //     return obj;
-    // }
-    // function addPropertyV2(obj, key, value) {   
-    //      let idUser = {[key]: valu};    
-    //      return Object.assign(obj, idUser);
-    //     }
-    // function addPropertyV3(obj, key, value) {
-    //     let idUser = {[key]: value};
-    //     return Object.assign({}, obj, idUser);
-    // }
-    // function addPropertyV4(obj, key, value) {
-    //     let idUser = {[key]: value};
-    //     return Object.assign({...obj }, idUser);
-    // }
+
+function addPropertyV4(obj, key, value) {
+    let idUser = {
+        [key]: value };
+    return Object.assign({...obj }, idUser);
+}
 
 const transaction = {
     value: 170,
