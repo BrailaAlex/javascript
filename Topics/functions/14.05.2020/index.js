2dcbbf59ed0ab1b853a13dd328bc21a17dce5444
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-14 10:39:09 +0300

    Func

---
+++ Topics/functions/14.05.2020/index.js
@@ -0,0 +1,74 @@
+/*
+ * ф-ция getSenseOfLife не должна принимать аргументы
+ * ф-ция getSenseOfLife должна вернуть число 42
+ */
+// ... code here
+
+function getSenseOfLife() {
+    return 42;
+}
+
+
+/* ф-ция getSquared должна принимать число и вернуть квадрат этого числа */
+// ... code here
+/* input: type of data
+/* output: type of data
+*/
+function getSquared(num) {
+    return num * num;
+}
+
+
+/* ф-ция sum должна принимать два числа и вернуть их сумму */
+// ... code here
+
+/* input: number
+/* output: number
+*/
+const sum = (a, b) => a + b;
+
+
+/*
+ * ф-ция sum должна принимать число
+ * ф-ция sum должна вывести в консоль строку 'I am 34 years old'
+ * где 34 - число, полученное в аргументах
+ */
+// ... code here
+
+/* input: number
+/* output: undefined
+*/
+const prinAge = num => console.log('I am ' + num + ' years old');
+
+
+/* ======> Arrow functions <======= */
+
+/* ф-ция mult должна принимать два числа и вернуть их произведение */
+// ... code here
+
+/* input: number, number
+/* output: number
+*/
+const mult = (a, b) => a * b;
+
+
+/* ф-ция square должна принимать число и вернуть его, возведенное в квадрат */
+// ... code here
+
+/* input: number
+/* output: number
+*/
+const square = a => a * a;
+console.log('Hello');
+
+
+/*
+ * ф-ция getMagicNumber не должна принимать аргументы
+ * ф-ция getMagicNumber должна вернуть число 17
+ */
+// ... code here
+
+/* input: -
+/* output: number
+*/
+const getMagicNumber = () => 17;
