ec5afcbe1d4c98c1640a3da0b3922185ed3134cf
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-14 16:57:49 +0300

    L10

--- Lesson10/HW2/index.js
+++ Lesson10/HW2/index.js
@@ -12,18 +12,8 @@ const getRandomNumbers = (length, start, end) => {
     })
 }
 
-console.log(getRandomNumbers(7, 0.2, 1.1));
-// const getRandomNumbers = (length, start, end) => {
-//     const noNumbersBetween = (end - start < 1) && Math.ceil(end) === Math.ceil(start);
-//     if (start > end || noNumbersBetween) return null;
+console.log(getRandomNumbers(7, -33, 22));
 
-
-//     return Array(length)
-//         .fill().map(i => Math.floor(Math.random() * (end - start)) + start);
-// }
-
-
-// console.log(getRandomNumbers(5, -1, 3));
 
 
 
