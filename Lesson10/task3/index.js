4df47d6d25d4e87b91cae07607b1a53ed79104d7
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-14 14:40:12 +0300

    L10

--- Lesson10/task3/index.js
+++ Lesson10/task3/index.js
@@ -1,15 +1,13 @@
 // input: number
 // output: array
 
-const multiRound = num => {
-    return matharray = [
-        Math.floor(num * 100) / 100,
-        Math.round(num * 100) / 100,
-        Math.ceil(num * 100) / 100,
-        Math.trunc(num * 100) / 100, +num.toFixed(2)
-    ]
-}
+const multiRound = num => matharray = [
+    Math.floor(num * 100) / 100,
+    Math.round(num * 100) / 100,
+    Math.ceil(num * 100) / 100,
+    Math.trunc(num * 100) / 100, +num.toFixed(2)
+]
 
-const num = -17.5555;
+let num = -17.5555;
 let result = multiRound(num);
 console.log(result)
