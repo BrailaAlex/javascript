55825d4bb2d4b4aa21c6ac7514e8cb1bd90ca11e
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-14 15:53:11 +0300

    Д10

--- Lesson10/HW1/index.js
+++ Lesson10/HW1/index.js
@@ -1,10 +1,8 @@
 // input: array
 // output: number
 
-const getTotalPrice = arr => {
-    const arrSum = arr.reduce((acc, num) => acc += num);
-    return sum = '$' + Math.floor(arrSum * 100) / 100;
-}
+const getTotalPrice = arr => '$' + Math
+    .floor(arr.reduce((acc, num) => acc += num, 0) * 100) / 100;
 
 const arr = [17.256, 17.35, 23];
 let result = getTotalPrice(arr)
