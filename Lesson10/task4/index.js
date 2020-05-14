60fcd90c1e79f6a3a7bb6dd7a851d59f4e4f78ca
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-14 15:34:00 +0300

    L10

--- Lesson10/task4/index.js
+++ Lesson10/task4/index.js
@@ -15,12 +15,19 @@
 
 // }
 
+// const getMaxAbsoluteNumber = arr => {
+//     if (!Array.isArray(arr) || (arr.length === 0)) return null;
+//     const absoluteValues = arr
+//         .map(num => Math.abs(num));
+
+//     return max = Math.max(...absoluteValues);
+// }
+
 const getMaxAbsoluteNumber = arr => {
-    if (!Array.isArray(arr) || (arr.length === 0)) return null;
+    if (!Array.isArray(arr) || arr.length === 0) return null;
     const absoluteValues = arr
         .map(num => Math.abs(num));
-
-    return max = Math.max(...absoluteValues);
+    return Math.max(...absoluteValues);
 }
 
 // input: array
