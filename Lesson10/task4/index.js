6d824438d35b2790f98b7a2afb6a64d0b5af8db0
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-14 15:30:47 +0300

    L10

--- Lesson10/task4/index.js
+++ Lesson10/task4/index.js
@@ -15,22 +15,22 @@
 
 // }
 
-const getMaxAbsoluteNumber = arr => {
-    if (!Array.isArray(arr) || (arr.length == 0)) return null;
-    const absoluteValues = arr
-        .map(num => Math.abs(num));
+// const getMaxAbsoluteNumber = arr => {
+//     if (!Array.isArray(arr) || (arr.length == 0)) return null;
+//     const absoluteValues = arr
+//         .map(num => Math.abs(num));
 
-    return max = Math.max(...absoluteValues);
-}
+//     return max = Math.max(...absoluteValues);
+// }
 
 // input: array
 // output: max abs number
-// const getMaxAbsoluteNumber = arr => {
-//         if (!Array.isArray(arr) || (arr.length == 0)) return null;
-//         return absMax = arr.reduce((a, b) => Math.max(Math.abs(a), Math.abs(b)));
-//     }
-// 1 - get absolute values
-// 2 - find max
+const getMaxAbsoluteNumber = arr => {
+        if (!Array.isArray(arr) || (arr.length === 0)) return null;
+        return absMax = arr.reduce((a, b) => Math.max(Math.abs(a), Math.abs(b)));
+    }
+    // 1 - get absolute values
+    // 2 - find max
 
 let arr = [-7, 2, -5, -78, 32, -99, -101.35];
 console.log(getMaxAbsoluteNumber(arr));
