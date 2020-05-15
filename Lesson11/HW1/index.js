a34cfb7b1722362e9046c0015f8f1f305b218986
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-15 14:44:57 +0300

    L11

--- Lesson11/HW1/index.js
+++ Lesson11/HW1/index.js
@@ -1,10 +1,25 @@
-// input: number
-// outptut: array
+// input: String;
+// output: array which cinsists of String elements with titled length;
 
-const superRound = (num, accuracy) => [
-    Math.floor((num) * (10 ** accuracy)) / 10 ** accuracy,
-    Math.round((num) * (10 ** accuracy)) / 10 ** accuracy,
-    Math.ceil((num) * (10 ** accuracy)) / 10 ** accuracy,
-    Math.trunc((num) * (10 ** accuracy)) / 10 ** accuracy, +num.toFixed(accuracy)
-]
-console.log(superRound(7.356, 2));
+const text = 'These arguments customize the behavior of the function and let app';
+// const text = 55
+const length = 15;
+
+const splitString = (string, len = 10) => {
+    if (typeof string !== 'string') return null
+    let strArr = [];
+    elemStart = 0;
+    while (true) {
+        let strArrElem = string.substr(elemStart, len);
+        if (strArrElem.length === 0) break;
+        if (strArrElem.length < len) {
+            strArrElem = strArrElem + '.'.repeat(len - strArrElem.length)
+        }
+        elemStart += len;
+        strArr.push(strArrElem);
+    }
+    return strArr;
+}
+
+const res = splitString(text, length)
+console.log(res);
