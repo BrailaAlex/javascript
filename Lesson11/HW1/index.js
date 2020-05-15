303c2624bc640e7404cd5e36f701632bfccae430
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-15 14:57:49 +0300

    L11

--- Lesson11/HW1/index.js
+++ Lesson11/HW1/index.js
@@ -3,27 +3,49 @@
 
 
 
-const splitString = (text, len = 10) => {
-    if (typeof string !== 'string') return null
-    let strArr = [];
-    let elemStart = 0;
+// const splitString = (text, len = 10) => {
+//     if (typeof string !== 'string') return null
+//     let strArr = [];
+//     let elemStart = 0;
+//     while (true) {
+//         let strArrElem = text.substr(elemStart, len);
+//         if (strArrElem.length === 0) {
+//             break;
+//         };
+//         if (strArrElem.length < len) {
+//             strArrElem = strArrElem + '.'.repeat(len - strArrElem.length);
+//         };
+//         strArr.push(strArrElem);
+//         elemStart += len;
+//     }
+//     return strArr;
+// }
+
+const splitString = (text, num = 10) => {
+    if (typeof text !== 'string') return null;
+
+    const arr = [];
+    let startPosition = 0;
+
     while (true) {
-        let strArrElem = text.substr(elemStart, len);
-        if (strArrElem.length === 0) {
+
+        let chunk = text.substr(startPosition, num);
+
+        if (chunk.length === 0) {
             break;
+        }
+        if (chunk.length < num) {
+            chunk = chunk + '.'.repeat(num - chunk.length);
         };
-        if (strArrElem.length < len) {
-            strArrElem = strArrElem + '.'.repeat(len - strArrElem.length);
-        };
-        strArr.push(strArrElem);
-        elemStart += len;
+        arr.push(chunk);
+        startPosition += num;
     }
-    return strArr;
+
+    return arr;
 }
+const text = 'These arguments customize the behavior of the function and let app';
+// const text = 55
+const length = 15;
 
-// const text = 'These arguments customize the behavior of the function and let app';
-// // const text = 55
-// const length = 15;
-
-// const res = splitString(text, length)
-// console.log(res);
+const res = splitString(text, length)
+console.log(res);
