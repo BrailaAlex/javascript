143ada2b561e42261ff53cfab494e805220976eb
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-15 15:40:41 +0300

    L11

--- Lesson11/HW2/index.js
+++ Lesson11/HW2/index.js
@@ -5,10 +5,9 @@ const countOccurrences = (str1, elem) => {
     if (elem.length === 0) return null;
     let midArray = str1.split(elem);
     return occurences = midArray.length - 1;
-    // let occurences = str1.match(elem)   
 }
 
-const string1 = 'language whose formatting conventions should';
-const string2 = 't'
-let res = countOccurrences(string1, string2)
-console.log(res);
+// const string1 = 'language whose formatting conventions should';
+// const string2 = 't'
+// let res = countOccurrences(string1, string2)
+// console.log(res);
