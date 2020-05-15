68d37f1b8fc9f47d8253fbb2b95a2884d2509a5f
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-15 18:22:03 +0300

    L12

---
+++ Lesson12/task1/index.js
@@ -0,0 +1,26 @@
+// calc
+
+//'1 + 2' => '1 + 2 = 3'
+
+
+const calc = expression => {
+    const [a, operator, b] = expression.split(' '); //используем деструктуризацию массива присваивая каждому 
+    let result; //используем перпеменную, чтобы в нее помещать результат      //элементу имя переменной
+    switch (operator) {
+        case '+':
+            result = +a + Number(b);
+            break;
+        case '-':
+            result = a - b;
+            break;
+        case '*':
+            result = a * b;
+            break;
+        case '/':
+            result = a / b;
+            break;
+    }
+    return `${expression}  = ${result}`;
+};
+const expr = '22 / 35'
+console.log(calc(expr));
