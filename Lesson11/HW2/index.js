b6de16d7e3bf935a07d4bc5365adf7db030bbb4b
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-15 15:36:07 +0300

    L11

--- Lesson11/HW2/index.js
+++ Lesson11/HW2/index.js
@@ -1,13 +1,13 @@
 // input: string!, strin2;
 // output: number = quantity of entries of string2 in string1;
 
-const string1 = 'language whose formatting conventions should';
-const string2 = 'a'
 const countOccurences = (str1, elem) => {
     if (elem.length === 0) return null;
     return occurences = str1.split(elem).length - 1;
     // let occurences = str1.match(elem)   
 }
 
-let res = countOccurences(string1, string2)
-console.log(res);
+// const string1 = 'language whose formatting conventions should';
+// const string2 = 'a'
+// let res = countOccurences(string1, string2)
+// console.log(res);
