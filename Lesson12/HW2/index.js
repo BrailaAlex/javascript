1c7eb38877ab16b93f5e2ab6748dfeb3e9b3c0f9
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-15 19:32:00 +0300

    L12

--- Lesson12/HW2/index.js
+++ Lesson12/HW2/index.js
@@ -1,9 +1,11 @@
 // input: Array
 // output: array with cleaned elements
 
-const cleanTransactionslist = arr => arr
-    .filter(num => typeof + num === 'number' && !isNaN(num))
-    .map(num => '$' + (+num).toFixed(2));
+const cleanTransactionslist = arr => {
+    arr
+        .filter(num => typeof + num === 'number' && !isNaN(num))
+        .map(num => '$' + (+num).toFixed(2));
+}
 
 // const array = ['  1.9', '2.75 dollar', '3.25hryvna', '  2.758  '];
 // const res = cleanTransactionslist(array);
