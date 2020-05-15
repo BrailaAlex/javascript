d581505cc9bb13ac9725ac65aec42d701ad41971
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-15 12:56:07 +0300

    L11

---
+++ Lesson11/task1/index.js
@@ -0,0 +1,37 @@
+// const text = 'message'
+
+// const myMessage = `
+// The text is ${text.toUpperCase()},
+// And something Else.
+// `;
+// const myMessage = `The text is ${text.toUpperCase()},
+// And something Else.`;
+
+// console.log(text.charAt(10));
+
+const someText = 'Lorem ipsum is simply dummy text of the printing and typesetting';
+const length = 15;
+//splitText
+// splitString('abcd efgh', 4) => 'Abcd\n efg\nH']
+
+const splitText = (text, len) => {
+    if (typeof text !== 'string') return null;
+    const strArr = [];
+    let startPosition = 0
+    while (true) {
+        let chunk = text.substr(startPosition, len)
+        if (chunk.length === 0) { //проверка осуществляется перед действиями с эл-ом
+            break; //чтобы не вызвать ошибку при работе с несуществующим эл-ом
+        }
+        // strArr.push(chunk)
+        strArr.push(chunk[0].toUpperCase() + chunk.slice(1));
+        startPosition += len;
+
+    }
+    return strArr.join('\n');
+    // return strArr.map(num => num[0].toUpperCase() + num.slice(1)).join('\n');
+};
+const res = splitText(someText, length);
+console.log(res);
+// 1. 'abcdefg', 4 => ['abcd', 'efg'] => ['Abcd', 'Efg']
+// => Abcd/nEfg
