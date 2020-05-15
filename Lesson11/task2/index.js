a1d8f22ad9671eb3836b9c0b285267cf20fe3503
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-15 13:07:12 +0300

    L11

---
+++ Lesson11/task2/index.js
@@ -0,0 +1,37 @@
+// sortContacts
+
+const contacts = [{
+        name: 'Tom',
+        phoneNumber: '666-66-66',
+    },
+    {
+        name: 'Bob',
+        phoneNumber: '555-55-55',
+    },
+    {
+        name: 'Tim',
+        phoneNumber: '444-44-44',
+    },
+    {
+        name: 'Ann',
+        phoneNumber: '333-33-33',
+    },
+    {
+        name: 'Al',
+        phoneNumber: '222-22-22',
+    },
+
+];
+
+// input: array of objects
+// output: Sorted by name order list of contacts
+
+const sortContacts = (contacts, isAsc = true) => {
+    if (!Array.isArray(contacts))
+        if (!isAsc) return contacts
+            .sort((a, b) => b.name.localeCompare(a.name));
+    return contacts.
+    sort((a, b) => a.name.localeCompare(b.name));
+
+}
+console.log(sortContacts(contacts));
