3d4881f0e40ad10b2e2abe145d661a45509d1388
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-16 19:13:09 +0300

    l13

--- Lesson13/task2/calculator.test.js
+++ Lesson13/task2/calculator.test.js
@@ -1,17 +1,16 @@
-import { getSquaredNumbers, GetOddNumbers } from './calculator.js'
-// const getSquaredNumbers = arr =>
-//     arr.map(num => num * num);
+import { getSquaredArray, GetOddNumbers } from './calculator.js'
+import getSum from './calculator.js'
+
+it('should retern square numbers of array', () => {
+    const result = getSquaredArray([1, 2, 3]);
+    expect(result).toEqual([1, 4, 9]);
+});
 
 it('should keep odd numbers only', () => {
     const result = GetOddNumbers([1, 2, 3]);
     expect(result).toEqual([1, 3]);
 });
-it('should retern square numbers of array', () => {
-    const result = getSquaredNumbers([1, 2, 3]);
-    expect(result).toEqual([1, 4, 9]);
-});
 
-import getSum from './calculator.js'
 
 it('shoulf return sum of array elements', () => {
     const sumOfElements = getSum(6, 5);
