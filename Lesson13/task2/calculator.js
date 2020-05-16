c60c0b1f1e611c1e36cd60e56a9dded805e2b5ae
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-16 19:14:43 +0300

    L13

--- Lesson13/task2/calculator.js
+++ Lesson13/task2/calculator.js
@@ -4,6 +4,6 @@ export const getSquaredArray = arr =>
     arr.map(num => num * num);
 
 export const GetOddNumbers = arr =>
-    arr.filter(num => num % 2 === 1);
+    arr.filter(num => num % 2 !== 0);
 
 export default (a, b) => a + b;
