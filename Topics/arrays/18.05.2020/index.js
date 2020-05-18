b08495523017ffce538d60b25ac161ab36a8799b
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-18 10:19:52 +0300

    top

---
+++ Topics/arrays/18.05.2020/index.js
@@ -0,0 +1,22 @@
+//input: array of numbers;
+//output: array with squared numbers;
+
+function squareArray1(arr) {
+    if (!Array.isArray) return null;
+    //1 - good
+    let res = []; //создаем новый пустой массив
+
+    for (let el of arr) {
+        res.push(el * el);
+    }
+    return res;
+}
+
+// function squareArray2(arr) {
+//     if (!Array.isArray) return null;
+//     let res = [...arr];
+//     for (let i = 0; i < arr.length; i++) {
+//         res[i] = arr[i] * arr[i];
+//     }
+//     return res;
+// }
