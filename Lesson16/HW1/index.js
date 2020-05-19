d1519bf4cf68d331b2c044c4f9e8ce9d8220d954
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-19 15:35:26 +0300

    L16

--- Lesson16/HW1/index.js
+++ Lesson16/HW1/index.js
@@ -1,9 +1,12 @@
 //factory of arrays
 
 export const createArrayOfFunctions = (num) => {
-    if (typeof num !== 'number') return null;
-    let arrOfFunctions = [];
     if (num == undefined) return arrOfFunctions;
+
+    if (typeof num !== 'number')
+        return null;
+
+    let arrOfFunctions = [];
     for (let i = 0; i < num; i++) {
         // arrOfFunctions.push(el => i);
         arrOfFunctions[i] = () => i
