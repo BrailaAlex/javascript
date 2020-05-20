6a26c2e9f1bad3c8402acc7486f8b4bc1998a884
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-20 11:21:14 +0300

    L17

--- Lesson17/task2/index.js
+++ Lesson17/task2/index.js
@@ -1,16 +1,14 @@
 //callbackPrompt
 
+
 export const callbackPrompt = {
     message: 'Tell me your number',
     showPrompt() {
-        // console.log(this);
         const phoneNumber = prompt(this.message);
         console.log(phoneNumber);
     },
     showDeferredPrompt(ms) {
-        // console.log(this);
-        setTimeout(this.showPrompt.bind(this), ms)
+        setTimeout(this.showPrompt.bind(this), ms);
     }
 };
-
 // callbackPrompt.showDefferedPrompt(1000);
