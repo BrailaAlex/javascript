2f12679556f921805845e858e5ea0fd92baaf99c
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-20 19:35:42 +0300

    L17

---
+++ Lesson17/HW5/index.js
@@ -0,0 +1,25 @@
+export const timer = {
+    secondsPassed: 0,
+    minsPassed: 0,
+    timerID: 0,
+    starttimer() {
+        this.timerID = setInterval(() => {
+            this.secondsPassed += 1;
+            if (this.secondsPassed == 60) {
+                this.minsPassed++;
+                this.secondsPassed = 0;
+            }
+        }, 1000)
+    },
+    getTime() {
+        return (`${this.minsPassed}:${this.secondsPassed < 10 ? '0' + this.secondsPassed : this.secondsPassed}`)
+    },
+    clearInterval() {
+        this.clearInterval(this.timerID)
+    },
+    resetTimer() {
+        this.minsPassed = 0;
+        this.secondsPassed = 0;
+    }
+
+}
