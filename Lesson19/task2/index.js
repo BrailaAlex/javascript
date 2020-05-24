d7d51fcda2559711a32143b621236b3c5135dbbc
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-24 17:02:24 +0300

    L19

--- Lesson19/task2/index.js
+++ Lesson19/task2/index.js
@@ -1,7 +1,7 @@
 export function getOwnProps(obj) {
     const propArr = [];
     for (const prop in obj) {
-        if (obj.hasOwnProperty(prop) && typeof obj !== 'function') {
+        if (obj.hasOwnProperty(prop) && typeof obj[prop] !== 'function') {
             propArr.push(prop);
         }
     };
