29dabe88db93e77375a06a83a9dc3db76466caf8
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-24 16:57:07 +0300

    L19

--- Lesson19/task2/data.js
+++ Lesson19/task2/data.js
@@ -1,50 +1,49 @@
-const bicycle = {
-    name: 'kite',
-    ride() {
-        console.log(`${this.name} riding`);
-    },
-    stop() {
-        console.log(`${this.name} stopped`);
+// const bicycle = {
+//     name: 'kite',
+//     ride() {
+//         console.log(`${this.name} riding`);
+//     },
+//     stop() {
+//         console.log(`${this.name} stopped`);
 
-    }
-};
-// vehicle and ship
+//     }
+// };
+// // vehicle and ship
 
-const vehicle = {
-    name: 'BMW',
-    move() {
-        console.log(`${this.name} moving`);
-    },
-    stop() {
-        console.log(`${this.name} stopped`);
+// const vehicle = {
+//     name: 'BMW',
+//     move() {
+//         console.log(`${this.name} moving`);
+//     },
+//     stop() {
+//         console.log(`${this.name} stopped`);
 
-    },
-    __proto__: bicycle,
-};
+//     },
+//     __proto__: bicycle,
+// };
 
 
-const ship = {
-    name: 'Argo',
-    hasWheels: false,
-    liftAnchorDown() {
-        console.log('Lower the anchor');
-    },
-    liftAnchorUp() {
-        console.log('Lift the anchor');
-    },
-    __proto__: vehicle // vehicle now is a prototype for obj ship
-        //ship can now use parental methods from vehicle
-};
+// const ship = {
+//     name: 'Argo',
+//     hasWheels: false,
+//     liftAnchorDown() {
+//         console.log('Lower the anchor');
+//     },
+//     liftAnchorUp() {
+//         console.log('Lift the anchor');
+//     },
+//     __proto__: vehicle // vehicle now is a prototype for obj ship
+//         //ship can now use parental methods from vehicle
+// };
 
-// Object.setPrototypeOf(ship, vehicle);
-
-ship.ride();
-vehicle.move();
-console.log(ship);
-// ship.run(); //looking for nonexisting method => return error;
-for (let prop in ship) {
-    if (ship.hasOwnProperty(prop)) {
-        console.log(prop);
-    }
-};
-console.log(Object.keys(ship));
+export function getOwnProps(obj) {
+    let propArr = [];
+    for (let prop in obj) {
+        if (ship.hasOwnProperty(prop)) {
+            propArr.push(prop);
+        }
+    };
+    return propArr;
+}
+// console.log(Object.keys(ship));
+// console.log(getOwnProps(ship));
