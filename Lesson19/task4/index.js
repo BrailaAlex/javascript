27d1d9c42602f67e512dc48250197d886caa55b9
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-24 17:48:24 +0300

    L19

--- Lesson19/task4/index.js
+++ Lesson19/task4/index.js
@@ -1,20 +1,19 @@
-const user = {
+export const user = {
     firstName: 'John',
     lastName: 'Doe',
     get fullName() { //example of getter
         return `${this.firstName} ${this.lastName}`;
     },
-    // set fullName(value) { //example of setter
-    setFullName(value) { //analog of setter by simple method
-        console.log(value);
-        const [firstName, lastName] = value.split(' ');
+    set fullName(value) { //example of setter
+        // setFullName(value) { //analog of setter by simple method
+        const [firstName, lastName] = value.split(' '); // destructurizing of array
         this.firstName = firstName;
         this.lastName = lastName;
     },
 }
 
-console.log(user.fullName); //we refer to getter as to a property, not like to a method
-// user.fullName = 'Tom Hardy'; //setting a value for setter example
 // console.log(user.fullName); //we refer to getter as to a property, not like to a method
-user.setFullName('James Sallivan');
-console.log(user.firstName);
+// // user.fullName = 'Tom Hardy'; //setting a value for setter example
+// // console.log(user.fullName); //we refer to getter as to a property, not like to a method
+// user.setFullName('James Sallivan');
+// console.log(user.firstName);
