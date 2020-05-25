760fc40406aebc4aec4b27de4c8088e45ded1ccd
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-25 23:49:53 +0300

    L20

--- Lesson20/HW1/index.js
+++ Lesson20/HW1/index.js
@@ -1,6 +1,6 @@
 export class Order {
     constructor(price, city, type) {
-        this.id = Math.random.toString();
+        this.id = `${Math.random()}`
         this.price = price;
         this.dateCreated = new Date();
         this.isConfirmed = false;
@@ -10,7 +10,7 @@ export class Order {
     }
 
     checkPrice() {
-        if (this.prise > 1000) return true;
+        if (this.price > 1000) return true;
         return false;
     }
 
@@ -24,3 +24,15 @@ export class Order {
         return false;
     }
 }
+
+const order1 = new Order(250, 'Detroit', 'Buy');
+const order2 = new Order(250, 'Chicago', 'Buy');
+const order3 = new Order(250, 'Illinois', 'Buy');
+const order4 = new Order(250, 'Missisippi', 'Buy');
+const order5 = new Order(250, 'New York', 'Buy');
+console.log(order1);
+console.log(order2);
+console.log(order3);
+console.log(order4);
+console.log(order5);
+// console.log(Order.checkPrice());
