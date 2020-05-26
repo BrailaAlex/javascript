98ba0227f2113f12ff47a5d2678dd1e05bb8644c
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-26 11:22:31 +0300

    L20

--- Lesson20/HW2/index.js
+++ Lesson20/HW2/index.js
@@ -3,21 +3,26 @@ class User {
         this._id = `${id}`;
         this._name = `${name}`;
         this._sessionId = `${sessionId}`;
-    }
+    };
 };
 
 class UserRepository {
-    constructor(users) {
-        this._users = Object.freeze(users);
-    }
+    constructor(arr) {
+        this._users = Object.freeze(arr);
+    };
+
     getUserNames() {
-        return this._users.map(obj => obj._name);
-    }
+        return this._users.map(i => i._name);
+    };
+
     getUserIds() {
-        return this._users.map(obj => obj._id);
-    }
-    getUserNameById(id) {
-        return this._users.find(obj => obj._id === id).name;
-    }
+        return this._users.map(i => i._id);
+    };
+
+    getUserNameById(userId) {
+        let findById = this._users.find(i => i._id === userId);
+        return findById === undefined ? [] : findById._name;
+    };
 };
+
 export { User, UserRepository };
