c348d5a96b16ca7c6aa53db2bf8cd976698b9639
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-26 21:45:15 +0300

    L21

---
+++ Lesson21/HW1/index.js
@@ -0,0 +1,10 @@
+export const getTitleElement = () => {
+    const titleElement = document.quarySelector('.title');
+    console.dir(titleElement);
+    return titleElement;
+}
+export const getInputElement = () => {
+    const inputElement = document.quarySelector('text');
+    console.dir(inputElement);
+    return inputElement;
+}
