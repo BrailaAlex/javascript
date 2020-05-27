0b17dd60df37d97808aa98ed0126dbedf1ebbc0c
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-27 09:16:12 +0300

    L21

---
+++ Lesson21/HW3/index.js
@@ -0,0 +1,19 @@
+const getTitle = () => {
+    return document.querySelector('.title').textContent;
+};
+const getDescription = () => {
+    return document.querySelector('.about').innerText;
+};
+const getPlans = () => {
+    return document.querySelector('.plans').innerHTML;
+};
+const getGoal = () => {
+    return document.querySelector('.goal').outerHTML;
+};
+
+// getTitle();
+// getDescription();
+// getPlans();
+// getGoal();
+
+export { getTitle, getDescription, getPlans, getGoal };
