6353ed83bad8e7b36c9116e54e2a53d0efc5530a
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-27 12:57:23 +0300

    L21

--- Lesson21/HW8/index.js
+++ Lesson21/HW8/index.js
@@ -1,18 +1,18 @@
 const finishList = () => {
-    const liElem1 = document.createElement('li');
-    const liElem4 = document.createElement('li');
-    const liElem6 = document.createElement('li');
-    const liElem8 = document.createElement('li');
-    liElem1.textContent = '1'
-    liElem4.textContent = '4'
-    liElem6.textContent = '6'
-    liElem8.textContent = '8'
+    const liElemFirst = document.createElement('li');
+    const liElemFourth = document.createElement('li');
+    const liElemSixth = document.createElement('li');
+    const liElemEightth = document.createElement('li');
+    liElemFirst.textContent = '1'
+    liElemFourth.textContent = '4'
+    liElemSixth.textContent = '6'
+    liElemEightth.textContent = '8'
     const listElem = document.querySelector('.list');
-    listElem.append(liElem8);
-    listElem.prepend(liElem1);
+    listElem.append(liElemEightth);
+    listElem.prepend(liElemFirst);
     const specLi = document.querySelector('.special');
-    specLi.before(liElem4);
-    specLi.after(liElem6);
+    specLi.before(liElemFourth);
+    specLi.after(liElemSixth);
 };
 finishList();
 export { finishList };
