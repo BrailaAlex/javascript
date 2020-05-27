14ed7d8dcac19644aeb295f102b82d89af99ad8d
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-27 11:56:46 +0300

    L21

--- Lesson21/HW5/index.js
+++ Lesson21/HW5/index.js
@@ -1,5 +1,5 @@
 const setButton = buttonText => {
-        return document.querySelector('body').innerHTML = `<button>${buttonText}</button>`;
-    }
-    // setButton("dfvtf");
+    return document.querySelector('body').innerHTML = `<button>${buttonText}</button>`;
+}
+setButton("button text");
 export { setButton };
