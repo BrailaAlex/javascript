041c94eeddec4ffcc677e35e917fbace15ace2ff
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-27 12:22:00 +0300

    L21

--- Lesson21/HW7/index.js
+++ Lesson21/HW7/index.js
@@ -1,8 +1,8 @@
 const createButton = buttonText => {
-    const buttonElem = document.createElement('button').textContent = `${buttonText}`;
+    const buttonElem = document.createElement('button');
+    buttonElem.textContent = `${buttonText}`;
     return document.querySelector('body').append(buttonElem);
 };
-
 createButton('Send Email');
 
 export { createButton };
