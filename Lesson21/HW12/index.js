64edd910dbe5bf1a9d357dd51a8d1364937b18dd
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-27 19:18:20 +0300

    L21

--- Lesson21/HW12/index.js
+++ Lesson21/HW12/index.js
@@ -1,5 +1,6 @@
-const setTitle = text => {
-    return document.querySelector('.title').textContent = `${text}`;
-};
-
-export { setTitle };
+const getSection = num => {
+    const wantedSpan = document.querySelector(`span[data-number='${num}']`);
+    const parentDiv = wantedSpan.closest('.box');
+    return parentDiv.dataset.section;
+}
+getSection(5);
