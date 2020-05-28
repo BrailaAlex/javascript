f06da55c06d180f92e61f586b1e7a1f6cb0cda06
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-28 14:43:29 +0300

    L22

--- Lesson22/HW6/index.js
+++ Lesson22/HW6/index.js
@@ -1,7 +1,5 @@
 const btns = document.querySelectorAll('.btn');
-[jsBtn, htmlBtn] = btns;
 const handleclick = () => {
-    console.log(event.target.textContent);
-}
-jsBtn.addEventListener('click', handleclick);
-htmlBtn.addEventListener('click', handleclick);
+        console.log(event.target.textContent);
+    }
+    [...btns].map(el => el.addEventListener('click', handleclick));
