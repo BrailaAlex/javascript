d36175cd5c7d44018bbcb7b5d5b51c5dcda28107
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-28 13:40:13 +0300

    l22

--- Lesson22/HW3/index.js
+++ Lesson22/HW3/index.js
@@ -1,5 +1,4 @@
-const checkboxElem = document.querySelector('.task-status');
-// console.log(checkboxElem);
+export const checkboxElem = document.querySelector('.task-status');
 checkboxElem.addEventListener('change', () => {
     console.log(checkboxElem.checked);
 });
