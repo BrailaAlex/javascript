b5112a05e0f9b1660cecea34f4394e67b8125fcc
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-31 00:27:14 +0300

    L24

--- Lesson24/HW2/index.js
+++ Lesson24/HW2/index.js
@@ -1,29 +1,28 @@
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-const students = [
-    { name: 'Tom', birthDate: '01/15/2010' },
-    { name: 'Ben', birthDate: '02/21/2010' },
-    { name: 'Sam', birthDate: '01/16/2010' },
-    { name: 'Ann', birthDate: '03/12/2010' },
-    { name: 'Lilly', birthDate: '02/09/2010' },
-    { name: 'John', birthDate: '01/02/2010' },
-];
 
-export const studentsBirthDays = students => {
-        let result = {}
-        months.map(mon => {
-            result[mon] = [];
+const studentsBirthDays = students => {
+    let result = {};
+
+    months.map(arg => {
+        result[arg] = [];
+    });
+
+    students.sort((a, b) => new Date(a.birthDate).getDate() - new Date(b.birthDate).getDate())
+        .map(arg => {
+            const monthsName = months[new Date(arg.birthDate).getMonth()];
+            result[monthsName].push(arg.name);
         });
 
-        [...students].sort((a, b) => new Date(a.birthDate)
-            .getTime() - new Date(b.birthDate).getTime()).map(student => {
-            const month = months[new Date(student.birthDate).getMonth()]
-            result[month].push(student.name)
-        });
+    Object.keys(result).map(arg => {
+        if (result[arg].length !== 0) return;
 
-        Object.entries(result).map(el => {
-            if (el[1].length === 0)
-                delete result[el[0]];
-        });
-        return result;
-    }
-    // console.log(studentsBirthDays(students));
+        delete result[arg];
+    });
+
+
+    return result;
+};
+
+// console.log(studentsBirthDays(students));
+
+export { studentsBirthDays };
