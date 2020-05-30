8e8f34a2f415e869290c7a9962f613df23e4d25f
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-30 21:13:09 +0300

    l24

--- Lesson24/HW1/index.js
+++ Lesson24/HW1/index.js
@@ -1,11 +1,11 @@
 export const getDiff = (startDate, endDate) => {
-    const daysDiff = startDate.getDay() - endDate.getDay();
-    const hoursDiff = startDate.getHours() - endDate.getHours();
-    const minutesDiff = startDate.getMinutes() - endDate.getMinutes();
-    const secondsDiff = startDate.getSeconds() - endDate.getSeconds();
+    const daysDiff = endDate.getDay() - startDate.getDay();
+    const hoursDiff = endDate.getHours() - startDate.getHours();
+    const minutesDiff = endDate.getMinutes() - startDate.getMinutes();
+    const secondsDiff = endDate.getSeconds() - startDate.getSeconds();
     return `${daysDiff}d ${hoursDiff}h ${minutesDiff}m ${secondsDiff}s`
 }
 
-// startDate = new Date(2156185181);
-// endDate = new Date(2162185181);
-// console.log(getDiff(startDate, endDate));
+startDate = new Date(2156185181);
+endDate = new Date(2162185181);
+console.log(getDiff(startDate, endDate));
