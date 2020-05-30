0aaef70763d3877911a875b27c69c9aa2c5b621c
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-31 00:15:14 +0300

    L24

--- Lesson24/HW2/index.js
+++ Lesson24/HW2/index.js
@@ -1,12 +1,12 @@
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-const students = [
-    { name: 'Tom', birthDate: '01/15/2010' },
-    { name: 'Ben', birthDate: '02/21/2010' },
-    { name: 'Sam', birthDate: '01/16/2010' },
-    { name: 'Ann', birthDate: '03/12/2010' },
-    { name: 'Lilly', birthDate: '02/09/2010' },
-    { name: 'John', birthDate: '01/02/2010' },
-];
+// const students = [
+//     { name: 'Tom', birthDate: '01/15/2010' },
+//     { name: 'Ben', birthDate: '02/21/2010' },
+//     { name: 'Sam', birthDate: '01/16/2010' },
+//     { name: 'Ann', birthDate: '03/12/2010' },
+//     { name: 'Lilly', birthDate: '02/09/2010' },
+//     { name: 'John', birthDate: '01/02/2010' },
+// ];
 
 const studentsBirthDays = students => {
         const result = {}
