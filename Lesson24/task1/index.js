9cfa60c3f5863273d4bb9c63ab8b8b7a2198c28f
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-30 20:53:54 +0300

    L24

--- Lesson24/task1/index.js
+++ Lesson24/task1/index.js
@@ -1,6 +1,6 @@
 const weekDays = ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'];
 
-const dayOfWeek = (date, days) => {
+export const dayOfWeek = (date, days) => {
         const day = new Date(date).getDate(); // getting date (from 1 to 31) by getDate
         const dateInFuture = new Date(date).setDate(day + days); //here we will get a string of numbers which are miliseconds from 01.01.1970 03:00
         return weekDays[new Date(dateInFuture).getDay()]; // with method getDay we will receave a number of a day in a week from 0 to 6
