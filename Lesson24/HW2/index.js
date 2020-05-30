599b3f9beb4bc019d181c2bf9849fff47f968b24
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-31 00:23:42 +0300

    L24

--- Lesson24/HW2/index.js
+++ Lesson24/HW2/index.js
@@ -1,23 +1,21 @@
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-// const students = [
-//     { name: 'Tom', birthDate: '01/15/2010' },
-//     { name: 'Ben', birthDate: '02/21/2010' },
-//     { name: 'Sam', birthDate: '01/16/2010' },
-//     { name: 'Ann', birthDate: '03/12/2010' },
-//     { name: 'Lilly', birthDate: '02/09/2010' },
-//     { name: 'John', birthDate: '01/02/2010' },
-// ];
+const students = [
+    { name: 'Tom', birthDate: '01/15/2010' },
+    { name: 'Ben', birthDate: '02/21/2010' },
+    { name: 'Sam', birthDate: '01/16/2010' },
+    { name: 'Ann', birthDate: '03/12/2010' },
+    { name: 'Lilly', birthDate: '02/09/2010' },
+    { name: 'John', birthDate: '01/02/2010' },
+];
 
 export const studentsBirthDays = students => {
         let result = {}
-        sortedStudents = [...students].sort((a, b) => new Date(a.birthDate)
-            .getTime() - new Date(b.birthDate).getTime());
-
         months.map(mon => {
             result[mon] = [];
         });
 
-        sortedStudents.map(student => {
+        [...students].sort((a, b) => new Date(a.birthDate)
+            .getTime() - new Date(b.birthDate).getTime()).map(student => {
             const month = months[new Date(student.birthDate).getMonth()]
             result[month].push(student.name)
         });
@@ -28,4 +26,4 @@ export const studentsBirthDays = students => {
         });
         return result;
     }
-    // console.log(studentBirthDays(students));
+    // console.log(studentsBirthDays(students));
