0838786ddc9f69d4a3f0f9e6c36f4b4893e1b6e4
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-30 20:57:46 +0300

    L24

--- Lesson24/task2/index.js
+++ Lesson24/task2/index.js
@@ -5,6 +5,6 @@ const formatter = new Intl.DateTimeFormat('en', {
     hour12: false,
 });
 
-const getTime = date => formatter.format(date);
+export const getGreenwichTime = date => formatter.format(date);
 
-console.log(getTime(new Date()));
+console.log(getGreenwichTime(new Date()));
