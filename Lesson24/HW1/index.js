df7ed1fcbec54781981bcdac3dcd984365d03223
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-30 21:59:39 +0300

    L24

--- Lesson24/HW1/index.js
+++ Lesson24/HW1/index.js
@@ -1,11 +1,15 @@
-export const getDiff = (startDate, endDate) => {
-    const daysDiff = endDate.getDay() - startDate.getDay();
-    const hoursDiff = endDate.getHours() - startDate.getHours();
-    const minutesDiff = endDate.getMinutes() - startDate.getMinutes();
-    const secondsDiff = endDate.getSeconds() - startDate.getSeconds();
+const getDiff = (startDate, endDate) => {
+    msInDay = 1000 * 3600 * 24;
+    msInHour = 1000 * 3600;
+    msInMinute = 60000;
+    diffInMs = Math.abs(endDate.getTime() - startDate.getTime())
+    const daysDiff = Math.trunc(diffInMs / msInDay);
+    const hoursDiff = Math.trunc((diffInMs % msInDay) / msInHour);
+    const minutesDiff = Math.trunc((diffInMs % msInDay) % msInHour / msInMinute);
+    const secondsDiff = Math.trunc((diffInMs % msInDay) % msInHour % msInMinute / 1000);
     return `${daysDiff}d ${hoursDiff}h ${minutesDiff}m ${secondsDiff}s`
 }
 
-// startDate = new Date(2156185181);
-// endDate = new Date(2162185181);
-// console.log(getDiff(startDate, endDate));
+startDate = new Date(2019, 11, 3, 12, 20, 30);
+endDate = new Date(2019, 11, 7, 11, 40, 23);
+console.log(getDiff(startDate, endDate));
