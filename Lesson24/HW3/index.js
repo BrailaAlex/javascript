c203eee780eb40fc8cda65fce4732dc4c49c4d2f
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-05-31 16:55:50 +0300

    L24

--- Lesson24/HW3/index.js
+++ Lesson24/HW3/index.js
@@ -1,57 +1,55 @@
-const todos = [{
+const tasks = [{
         text: 'Buy milk',
         done: false,
-        id: 0,
-        date: new Date(2015, 9, 1, 0, 0, 0, 0),
-        dateСompleted: undefined,
+        id: 1,
+        createDate: new Date(2015, 9, 1, 0, 0, 0, 0),
+        doneDate: undefined,
     },
     {
         text: 'Pick up Tom from airport',
         done: false,
-        id: 1,
-        date: new Date(2016, 9, 1, 0, 0, 0, 0),
-        dateСompleted: undefined,
+        id: 2,
+        createDate: new Date(2016, 9, 1, 0, 0, 0, 0),
+        doneDate: undefined,
     },
     {
         text: 'Visit party',
         done: false,
-        id: 2,
-        date: new Date(2017, 9, 1, 0, 0, 0, 0),
-        dateСompleted: undefined,
-    },
-    {
-        text: 'Buy meat',
-        done: true,
-        id: 4,
-        date: new Date(2016, 9, 1, 0, 0, 0, 0),
-        dateСompleted: new Date(2017, 5, 3),
+        id: 3,
+        createDate: new Date(2016, 9, 1, 0, 0, 0, 0),
+        doneDate: undefined,
     },
     {
         text: 'Visit doctor',
         done: true,
-        id: 3,
-        date: new Date(2018, 9, 1, 0, 0, 0, 0),
-        dateСompleted: new Date(2019, 6, 4),
+        id: 4,
+        createDate: new Date(2016, 9, 1, 0, 0, 0, 0),
+        doneDate: new Date(2017, 5, 3),
+    },
+    {
+        text: 'Buy meat',
+        done: true,
+        id: 5,
+        createDate: new Date(2018, 9, 1, 0, 0, 0, 0),
+        doneDate: new Date(2019, 6, 4),
     },
 ];
-
-
 const listElem = document.querySelector('.list');
-const btnElem = document.querySelector('.btn');
+const crtBtnElem = document.querySelector('.create-task-btn');
 const inputElem = document.querySelector('.task-input');
 
 
 const renderListItems = listItems => {
     const listElem = document.querySelector('.list');
-
     const listItemsElems = listItems
-        .sort((a, b) => b.date - a.date)
+        .sort((a, b) => b.createDate - a.createDate)
         .sort((a, b) => a.done - b.done)
-        .sort((a, b) => b.dateСompleted - a.dateСompleted).map(({ text, done, id }) => {
+        .sort((a, b) => b.doneDate - a.doneDate)
+        .map(({ text, done, id }) => {
 
             const listItemElem = document.createElement('li');
             listItemElem.classList.add('list__item');
-            listItemElem.setAttribute('id', `${id}`);
+            listItemElem.dataset.id = `${id}`;
 
             const checkboxItem = document.createElement('input');
             checkboxItem.setAttribute('type', 'checkbox');
@@ -60,50 +58,46 @@ const renderListItems = listItems => {
                 listItemElem.classList.add('list__item_done');
             }
             checkboxItem.classList.add('list__item-checkbox');
-
-
             listItemElem.append(checkboxItem, text);
 
             return listItemElem;
         });
-
     listElem.append(...listItemsElems);
 
 };
 
-renderListItems(todos);
+renderListItems(tasks);
 
-const checkDoneWork = (event) => {
-    const checkTarget = event.target;
-    if (checkTarget.tagName != 'INPUT') return;
+const checkIfDone = (event) => {
+    const checkedElem = event.target;
+    if (checkedElem.tagName != 'INPUT') return;
 
-    const getId = todos.find(elem => elem.id === +checkTarget.parentElement.id);
-    getId.done = checkTarget.checked;
-    getId.dateСompleted = getId.done ? new Date() : undefined;
-
+    const getId = tasks.find(elem => elem.id === +checkedElem.parentElement.dataset.id);
+    getId.done = checkedElem.checked;
+    getId.doneDate = getId.done ? new Date() : undefined;
 
     listElem.innerHTML = '';
-    renderListItems(todos);
+    renderListItems(tasks);
 };
 
-listElem.addEventListener('click', checkDoneWork);
+listElem.addEventListener('click', checkIfDone);
 
 
-const creatNewWork = () => {
+const creatTask = () => {
     if (inputElem.value == '') return;
 
-    todos.unshift({
-        id: todos.length + 1,
+    tasks.push({
+        id: tasks.length + 1,
         text: inputElem.value,
         done: false,
-        date: new Date(),
-        dateCompleted: undefined,
+        createDate: new Date(),
+        checkDate: undefined,
     });
 
     inputElem.value = '';
 
     listElem.innerHTML = '';
-    renderListItems(todos);
+    renderListItems(tasks);
 };
 
-btnElem.addEventListener('click', creatNewWork);
+crtBtnElem.addEventListener('click', creatTask);
