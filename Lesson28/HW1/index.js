4bccdcc4930ab2c25006ade52052cebca8fac152
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-06-04 17:23:39 +0300

    L28

--- Lesson28/HW1/index.js
+++ Lesson28/HW1/index.js
@@ -1,33 +1,39 @@
-// const getDiff = (startDate, endDate) => {
-//     msInDay = 1000 * 3600 * 24;
-//     msInHour = 1000 * 3600;
-//     msInMinute = 60000;
-//     diffInMs = Math.abs(endDate.getTime() - startDate.getTime())
-//     const daysDiff = Math.trunc(diffInMs / msInDay);
-//     const hoursDiff = Math.trunc((diffInMs % msInDay) / msInHour);
-//     const minutesDiff = Math.trunc((diffInMs % msInDay) % msInHour / msInMinute);
-//     const secondsDiff = Math.trunc((diffInMs % msInDay) % msInHour % msInMinute / 1000);
-//     return `${daysDiff}d ${hoursDiff}h ${minutesDiff}m ${secondsDiff}s`
-// }
+export const shmoment = initValue => {
+    let actualDate = initValue;
+    console.log(actualDate);
+    const calculator = {
+        add(digit, value) {
+            let timeIncrease = {
+                milliseconds: (value) => (actualDate.setMilliseconds(actualDate.getMilliseconds() + value)),
+                seconds: (value) => (actualDate.setSeconds(actualDate.getSeconds() + value)),
+                minutes: (value) => (actualDate.setMinutes(actualDate.getMinutes() + value)),
+                hours: (value) => (actualDate.setHours(actualDate.getHours() + value)),
+                days: (value) => (actualDate.setDate(actualDate.getDate() + value)),
+                months: (value) => (actualDate.setMonth(actualDate.getMonth() + value)),
+                years: (value) => (actualDate.setFullYear(actualDate.getFullYear() + value)),
+            }
+            actualDate = new Date(timeIncrease[digit](value));
+            return calculator;
+        },
+        subtract(value) {
+            let timeDecrease = {
+                milliseconds: (value) => (actualDate.setMilliseconds(actualDate.getMilliseconds() - value)),
+                seconds: (value) => (actualDate.setSeconds(actualDate.getSeconds() - value)),
+                minutes: (value) => (actualDate.setMinutes(actualDate.getMinutes() - value)),
+                hours: (value) => (actualDate.setHours(actualDate.getHours() - value)),
+                days: (value) => (actualDate.setDate(actualDate.getDate() - value)),
+                months: (value) => (actualDate.setMonth(actualDate.getMonth() - value)),
+                years: (value) => (actualDate.setFullYear(actualDate.getFullYear() - value)),
+            }
+            actualDate = new Date(timeDecrease[digit](value));
+            return calculator;
+        },
+        result() {
+            return actualDate;
+        },
 
-function getDiff(startDate, endDate) {
-    const millSecPerMinute = 60 * 1000;
-    const millSecPerHour = 60 * millSecPerMinute;
-    const millSecPerDay = 24 * millSecPerHour;
-    const diffMs = Math.abs(endDate - startDate);
-    const countDays = Math.trunc(diffMs / millSecPerDay);
-    const restOfDayInMs = diffMs % millSecPerDay;
-    const countHours = Math.trunc((restOfDayInMs) / millSecPerHour);
-    const restOfHourInMs = restOfDayInMs % millSecPerHour;
-    const countMinutes = Math.trunc((restOfHourInMs) / millSecPerMinute);
-    const restOfMinutesInMs = restOfHourInMs % millSecPerMinute
-    const countSeconds = Math.trunc(restOfMinutesInMs / 1000);
+    };
+    return calculator;
+};
 
-    return `${countDays}d ${countHours}h ${countMinutes}m ${countSeconds}s`;
-}
-
-export { getDiff };
-
-// startDate = new Date(2019, 11, 3, 12, 20, 30);
-// endDate = new Date(2019, 11, 7, 11, 40, 23);
-// console.log(getDiff(startDate, endDate));
+// console.log(shmoment(new Date(2020, 0, 7, 17, 17, 17)).add('minutes', 17).result());
