ab728e317a78ea9a1d88c3112c69b95d63f8e03d
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-06-04 15:22:44 +0300

    L28

--- Lesson28/task2/index.js
+++ Lesson28/task2/index.js
@@ -1,25 +1,26 @@
-const favorites = ['id-6', 'id-7'];
+// const favorites = ['id-6', 'id-7'];
 
-const tree = {
-    id: 'id-1',
-    name: 'Products',
-    nodes: [{
-        id: 'id-6',
-        name: 'food',
-        nodes: [{
-            id: 'id-7',
-            name: "chps'",
-            nodes: []
-        }, ],
-    }, ],
-}
+// const tree = {
+//     id: 'id-1',
+//     name: 'Products',
+//     nodes: [{
+//         id: 'id-6',
+//         name: 'food',
+//         nodes: [{
+//             id: 'id-7',
+//             name: "chps'",
+//             nodes: []
+//         }, ],
+//     }, ],
+// }
 
 export const markFavorites = (tree, favorites) => {
     const isFavorite = favorites.includes(tree.id); //checks if node contains requested id on every step when we go deeper;
+
     return {
         ...tree,
         isFavorite,
-        nodes: tree.nodes.map(childNode => markFavorites(childNode, favourites))
+        nodes: tree.nodes.map(childNode => markFavorites(childNode, favorites)),
     }
 };
 // const result = markFavourites(tree, favourites);
