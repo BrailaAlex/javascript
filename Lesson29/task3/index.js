05711ec132449bc532b3fa8d84372074bd97f6b2
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-06-05 14:30:08 +0300

    L29

--- Lesson29/task3/index.js
+++ Lesson29/task3/index.js
@@ -1,10 +1,10 @@
-const main = () => {
+const printing = () => {
     console.log('1');
-    setTimeout(() => console.log('2'), 0);
-    console.log('3');
+    setTimeout(() => console.log('5'), 1000);
+    console.log('2');
     setTimeout(() => console.log('4'), 0);
-    console.log('5');
+    console.log('3');
 };
 
-main();
-console.log('6');
+// main();
+export { printing };
