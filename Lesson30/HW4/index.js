17e82a8b105a17626fa7e936fda293e81edb330b
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-06-06 20:27:45 +0300

    L30

---
+++ Lesson30/HW4/index.js
@@ -0,0 +1,42 @@
+const addImageV2 = url => {
+    return new Promise((resolve, reject) => {
+        const img = document.createElement('img');
+        img.setAttribute('alt', 'User avatar');
+        img.src = url;
+
+        const pageElem = document.querySelector('.page');
+        pageElem.append(img);
+
+        const onImageLoaded = () => {
+            const { width, height } = img;
+            resolve({ width, height });
+        }
+
+        img.addEventListener('load', onImageLoaded);
+
+        img.addEventListener('error', () => reject(new Error('Image load failed')));
+    });
+}
+const res = 'https://wallpapershome.com/images/wallpapers/rihan-1080x1920-top-music-artist-and-bands-singer-actress-510.jpg';
+const isPromise = addImageV2(res);
+isPromise.then(data => console.log(data));
+isPromise.catch(error => console.log(error));
+// const addImage = (url, callback) => {
+//     const img = document.createElement('img');
+//     img.setAttribute('alt', 'User avatar');
+//     img.src = url;
+
+//     const pageElem = document.querySelector('.page');
+//     pageElem.append(img);
+
+//     const onImageLoaded = () => {
+//         const { width, height } = img;
+//         callback(null, { width, height });
+//     }
+
+//     const onImageLoadError = () => callback('Image load failed');
+
+//     img.addEventListener('load', onImageLoaded);
+
+//     img.addEventListener('error', onImageLoadError);
+// };
