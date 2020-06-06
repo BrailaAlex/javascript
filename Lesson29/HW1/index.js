58bd84f278da689949fea6f1f28120a5592b2ffe
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-06-06 15:02:46 +0300

    L29

--- Lesson29/HW1/index.js
+++ Lesson29/HW1/index.js
@@ -9,14 +9,8 @@ const requestUserData = (userId, callback) => {
         email: userId + '@example.com'
     };
 
-    const callUser = setTimeout(() => callback(null, object), Math.floor(Math.random() * 3 + 1) * 1000);
+    const callUser = setTimeout(() => callback(object, null), Math.floor(Math.random() * 3 + 1) * 1000);
 }
-
-// function callback(error, data) {
-//     console.log(data);
-
-// }
-// requestUserData('userId777', callback);
 
 // const requestUserData = (userId, callback) => {
 //     if (userId == 'broken') {
