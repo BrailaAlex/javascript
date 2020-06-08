8c81a1dee2fde9086591d100399c3cc1a62b9e5a
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-06-08 11:32:35 +0300

    L30

--- Lesson30/HW4/index.js
+++ Lesson30/HW4/index.js
@@ -1,27 +1,27 @@
-export const addImageV2 = url => {
-    return new Promise(function addImage(resolve, reject) {
-        const img = document.createElement('img');
-        img.setAttribute('alt', 'User avatar');
-        img.src = url;
+const addImageV2 = url => {
+        return new Promise(function addImage(resolve, reject) {
+            const img = document.createElement('img');
+            img.setAttribute('alt', 'User avatar');
+            img.src = url;
 
-        const pageElem = document.querySelector('.page');
-        pageElem.append(img);
+            const pageElem = document.querySelector('.page');
+            pageElem.append(img);
 
-        const onImageLoaded = () => {
-            const { width, height } = img;
-            resolve({ width, height });
-        }
+            const onImageLoaded = () => {
+                const { width, height } = img;
+                resolve({ width, height });
+            }
 
-        img.addEventListener('load', onImageLoaded);
+            img.addEventListener('load', onImageLoaded);
 
-        const onImageLoadError = () => reject(new Error('Image load failed'))
+            const onImageLoadError = () => reject(new Error('Image load failed'))
 
-        img.addEventListener('error', onImageLoadError);
-    });
-}
-const res = 'https://wallpapershome.com/images/wallpapers/rihan-1080x1920-top-music-artist-and-bands-singer-actress-510.jpg';
-const isPromise = addImageV2(res);
-isPromise.then(data => console.log(data));
-isPromise.catch(error => console.log(error));
+            img.addEventListener('error', onImageLoadError);
+        });
+    }
+    // const res = 'https://wallpapershome.com/images/wallpapers/rihan-1080x1920-top-music-artist-and-bands-singer-actress-510.jpg';
+    // const isPromise = addImageV2(res);
+    // isPromise.then(data => console.log(data));
+    // isPromise.catch(error => console.log(error));
 
 export { addImageV2 };
