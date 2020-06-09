1fc20b5d201f5c3ee81125bd4ac6d19d12f57393
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-06-09 19:17:15 +0300

    L32

--- Lesson32/HW2/index.js
+++ Lesson32/HW2/index.js
@@ -1,6 +1,6 @@
-// const promiseNumber1 = Promise.resolve(67);
-// const promiseNumber2 = Promise.resolve(23);
-// const promiseNumber3 = Promise.resolve(8);
+const promiseNumber1 = Promise.resolve(67);
+const promiseNumber2 = Promise.resolve(23);
+const promiseNumber3 = Promise.resolve(8);
 
 /*
  * создайте промис и присвойте переменной resultPromise
@@ -8,14 +8,14 @@
  */
 const resultPromise = (...numbers) => Promise.all(numbers);
 
-// resultPromise(promiseNumber1, promiseNumber2, promiseNumber3)
-//     .then(numbersList => {
-//         console.log(numbersList);
-//         const sum = numbersList.reduce((acc, num) => acc + num, 0);
-//         return sum;
-//     })
-//     .then(result => {
-//         console.log(result); // 98
-//     });
+resultPromise(promiseNumber1, promiseNumber2, promiseNumber3)
+    .then(numbersList => {
+        console.log(numbersList);
+        const sum = numbersList.reduce((acc, num) => acc + num, 0);
+        return sum;
+    })
+    .then(result => {
+        console.log(result); // 98
+    });
 
-export { resultPromise };
+// export { resultPromise };
