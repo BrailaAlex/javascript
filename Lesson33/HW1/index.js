d655febcd6cd1e895340a614021afbb6d50dde5d
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-06-10 20:25:26 +0300

    L33

--- Lesson33/HW1/index.js
+++ Lesson33/HW1/index.js
@@ -4,15 +4,15 @@ const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks';
 const getTasksList = (sourceURL) => tasksList = fetch(sourceURL)
     .then(response => response.json());
 
-getTasksList(baseUrl)
-    .then(tasksList => console.log(tasksList));
+// getTasksList(baseUrl)
+//     .then(tasksList => console.log(tasksList));
 
 /* getTaskById code here */
 
-const getTaskById = taskId => getTasksList(baseUrl)
-    .then(tasksList => tasksList.find(task => task.id = taskId));
+const getTaskById = taskId => fetch(`${baseUrl}/${taskId}`)
+    .then(response => response.json());
 
 
-getTaskById(20)
-    .then(task => console.log(task));
+// getTaskById(20)
+//     .then(task => console.log(task));
 // export { getTasksList, getTaskById };
