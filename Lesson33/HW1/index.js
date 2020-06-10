3020d231ea8a4e6339225f53ed0189fdd5028e88
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-06-10 20:06:27 +0300

    L33

---
+++ Lesson33/HW1/index.js
@@ -0,0 +1,17 @@
+const baseUrl = 'https://5e5cf5eb97d2ea0014796f01.mockapi.io/api/v1/tasks';
+
+/* getTasksList code here */
+const getTasksList = (sourceURL) => tasksList = fetch(sourceURL)
+    .then(response => response.json());
+
+// getTasksList(baseUrl)
+//     .then(tasksList => console.log(tasksList));
+
+/* getTaskById code here */
+
+const getTaskById = taskId => getTasksList(baseUrl)
+    .then(tasksList => tasksList.find(task => task.id = taskId));
+
+
+// getTaskById(20)
+//     .then(task => console.log(task));
