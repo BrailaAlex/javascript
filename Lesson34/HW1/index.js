800e3b45767244ed739289ee74f70ac5097fb9f2
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-06-11 21:11:24 +0300

    L34

--- Lesson34/HW1/index.js
+++ Lesson34/HW1/index.js
@@ -6,39 +6,66 @@ const getUsersList = () => {
         .then(response => response.json());
 }
 
+// getUsersList()
+//     .then(value => console.log(value));
+
 /* getUserById code here */
 const getUserById = UserId => {
-    return fetch(`${rootUrl}/${UserId}`)
-        .then(response => response.json());
-}
+        return fetch(`${rootUrl}/${UserId}`)
+            .then(response => response.json());
+    }
+    // getUserById(5)
+    //     .then(value => console.log(value));
 
 /* createUser code here */
 const createUser = userObj => {
-    return fetch(rootUrl, baseUrl, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8',
-        },
-        body: JSON.stringify(userObj)
-    });
-}
+        return fetch(rootUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8',
+            },
+            body: JSON.stringify(userObj)
+        });
+    }
+    // const userObj = {
+    //     email: "mint.Kemmer@hotmail.com",
+    //     firstName: "Ann",
+    //     lastName: "Hethaway",
+    //     age: 52
+    // };
+    // createUser(userObj);
+    // getUsersList()
+    //     .then(value => console.log(value));
 
 /* updateUser code here */
 
 const updateUser = (UserId, userObj) => {
-    return fetch(`${rootUrl}/${UserId}`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json;charset=utf-8',
-        },
-        body: JSON.stringify(userObj)
-    });
-}
+        return fetch(`${rootUrl}/${UserId}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8',
+            },
+            body: JSON.stringify(userObj)
+        });
+    }
+    // const userObj2 = {
+    //     id: "9",
+    //     email: "monana.Kemmer@hotmail.com",
+    //     firstName: "Monana",
+    //     lastName: "Geller",
+    //     age: 27
+    // };
+    // updateUser(7, userObj2);
+    // getUsersList()
+    //     .then(value => console.log(value));
 
 /* deleteUser code here */
 
 const deleteUser = UserId => {
-    return fetch(`${rootUrl}/${UserId}`, {
-        method: 'DELETE',
-    });
-}
+        return fetch(`${rootUrl}/${UserId}`, {
+            method: 'DELETE',
+        });
+    }
+    // deleteUser(13);
+
+export { deleteUser, updateUser, createUser, getUserById, getUsersList };
