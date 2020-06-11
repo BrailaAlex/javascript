b27b2619ba2d6bfa4ad7c9e864a35afca06bf8ea
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-06-11 21:59:20 +0300

    L34

--- Lesson34/HW1/index.js
+++ Lesson34/HW1/index.js
@@ -1,31 +1,41 @@
-// rootUrl = 'https://5ee220bb8b27f3001609462e.mockapi.io/api/v1/users';
+rootUrl = 'https://5ee220bb8b27f3001609462e.mockapi.io/api/v1/users';
 
 /* getUsersList code here */
-export const getUsersList = () => fetch(rootUrl)
-    .then(response => response.json());
+export function getUsersList() {
+    return fetch(rootUrl)
+        .then(response => response.json())
+};
 
 /* getUserById code here */
-export const getUserById = UserId => fetch(`${rootUrl}/${UserId}`)
-    .then(response => response.json());
+export function getUserById(UserId) {
+    return fetch(`${rootUrl}/${UserId}`)
+        .then(response => response.json())
+};
 
-export const createUser = userObj => fetch(rootUrl, {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json;charset=utf-8',
-    },
-    body: JSON.stringify(userObj)
-});
+export function createUser(userObj) {
+    return fetch(rootUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8',
+        },
+        body: JSON.stringify(userObj)
+    })
+};
 
-export const updateUser = (UserId, userObj) => fetch(`${rootUrl}/${UserId}`, {
-    method: 'PUT',
-    headers: {
-        'Content-Type': 'application/json;charset=utf-8',
-    },
-    body: JSON.stringify(userObj)
-});
+export function updateUser(UserId, userObj) {
+    return fetch(`${rootUrl}/${UserId}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8',
+        },
+        body: JSON.stringify(userObj)
+    })
+};
 
-export const deleteUser = UserId => fetch(`${rootUrl}/${UserId}`, {
-    method: 'DELETE',
-});
+export function deleteUser(UserId) {
+    return fetch(`${rootUrl}/${UserId}`, {
+        method: 'DELETE',
+    })
+};
 
 // export { deleteUser, updateUser, createUser, getUserById, getUsersList };
