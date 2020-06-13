ba3cbe2afa4e61dd3d9dbe116cb411cb28048606
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-06-13 17:35:18 +0300

    L35

--- Lesson35/task3/index.js
+++ Lesson35/task3/index.js
@@ -1,9 +1,41 @@
-export const printing = () => {
-    console.log(1);
-    setTimeout(function() { console.log(5); }, 1000);
-    console.log(2);
-    setTimeout(function() { console.log(4); }, 0);
-    console.log(3);
+import { renderUserData } from './user.js';
+import { renderRepos, cleanReposList } from './repos.js';
+import { fetchUserData, fetchRepositories } from './gateways.js';
+import { showSpinner, hideSpinner } from './spinner.js';
+
+
+const defaultUser = { //setting default user with default avatar and empty name and location
+    avatar_url: 'https://avatars3.githubusercontent.com/u10001',
+    name: '',
+    location: '',
 };
 
-// printing();
+renderUserData(defaultUser);
+
+// const defaultAvatar = 'https://api.github.com/users/USERNAME'
+//fetch and render user data ...
+
+
+const showUserBtnElem = document.querySelector('.name-form__btn');
+const userNameInputElem = document.querySelector('.name-form__input');
+
+
+const onSearchUser = () => {
+    cleanReposList();
+    showSpinner();
+    const userName = userNameInputElem.value;
+    fetchUserData(userName)
+        .then(userData => {
+            renderUserData(userData);
+            return userData.repos_url;
+        })
+        .then(url => {
+            return fetchRepositories(url);
+        })
+        .then(reposlist => renderRepos(reposlist))
+        .catch(error => alert(error.message))
+        .finally(() => hideSpinner())
+}
+
+showUserBtnElem.addEventListener('click', onSearchUser);
+//facebook
