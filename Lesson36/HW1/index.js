5c2a5f2d9e82991f5c174c6a129a69c8aadb6c27
BrailaAlex <60700132+BrailaAlex@users.noreply.github.com>
2020-06-14 15:40:12 +0300

    L36

--- Lesson36/HW1/index.js
+++ Lesson36/HW1/index.js
@@ -3,25 +3,32 @@
 //2. create function to request users data
 //3. use it for every user
 //4. reject promise if there is an error while request.
-const fetchUsers = async users => {
-    return await users.map(userId => fetch(`https://api.github.com/users/${userId}`)
-        .then(response => {
-            if (response.ok) return response.json();
-            throw new Error('Failed to load data');
-        })
-        .then(user => user.blog));
-}
 
-// const usersArr = ['facebook', 'google'];
+// const fetchUsers = users => {
+//     return await users.map(userId => fetch(`https://api.github.com/users/${userId}`)
+//         .then(response => {
+//             if (response.ok) return response.json();
+//             throw new Error('Failed to load data');
+//         })
+//         .then(user => user.blog));
+// }
+
+const usersArr = ['facebook', 'google'];
 const getUsersBlogs = async usersArr => {
     try {
-        const usersData = await Promise.all(fetchUsers(usersArr));
+        let users = await usersArr.map(userId => fetch(`https://api.github.com/users/${userId}`)
+            .then(response => {
+                if (response.ok) return response.json();
+                throw new Error('Failed to load data');
+            })
+            .then(user => user.blog));
+        const usersData = Promise.all(users);
         return usersData;
     } catch (err) {
         console.log(err);
     }
 };
-// getUsersBlogs(usersArr)
-//     .then(list => console.log(list));
+getUsersBlogs(usersArr)
+    .then(list => console.log(list));
 
 export { getUsersBlogs };
